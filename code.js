9ee687f4f5562dd510db31b9fc821bf15648d323
noorasif17 <162204402+noorasif17@users.noreply.github.com>
2024-03-24 14:31:29 -0600

    Day 8

---
+++ code.js
@@ -0,0 +1,33 @@
+// Question 25
+// let alien_colour = "Green";
+// if (alien_color == "Green"){
+//     console.log("You just earned 5 points!");
+// }
+// alien_colour = "Red";
+// if (alien_color == "Green"){
+// }
+// Question 26
+var alien_color = "Green";
+if (alien_color == "Green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+if (alien_color == "Yellow") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+// Question 27
+var alien_colour = "Blue";
+if (alien_colour == "Yellow") {
+    console.log("You earned 5 points");
+}
+else if (alien_colour == "Red") {
+    console.log("You earned 10 points");
+}
+else {
+    console.log("You earned 15 points");
+}
---
+++ code.ts
@@ -0,0 +1,36 @@
+// Question 25
+let aliencolor = "Green";
+if (aliencolor == "Green"){
+    console.log("You just earned 5 points!");
+}
+aliencolor = "Red";
+if (aliencolor == "Green"){
+
+}
+
+// Question 26
+let alien_color = "Green";
+if (alien_color == "Green"){
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+if (alien_color == "Yellow"){
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+
+// Question 27
+let alien_colour = "Blue";
+if (alien_colour == "Yellow"){
+    console.log("You earned 5 points");
+}
+else if(alien_colour == "Red"){
+    console.log("You earned 10 points");
+}
+else{
+    console.log("You earned 15 points");
+}
